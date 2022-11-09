a2b141a4c0f23be51eed44a0160adef9e15ee6f7
coduxer <coduxer@gmail.com>
2022-11-09 17:48:22 +0200

    lineage-patches: adapt cutout-force-full-screen to a13
    Change-Id: Ia0bc11a4e607b9224bdb408ffccb7ea48f57cba3

--- features/cutout_force_full_screen/apply-patch.sh
+++ features/cutout_force_full_screen/apply-patch.sh
@@ -7,7 +7,7 @@ ROOT_DIR="$SCRIPT_DIR/../.."
 source "$ROOT_DIR/utils.sh"
 
 
-cd "$ROOT_DIR/../lineageos/"
+cd "$ROOT_DIR/../../"
 
 apply-patch "frameworks/base" "$SCRIPT_DIR/android_frameworks_base--cutout-force-full-screen.patch"
 apply-patch "packages/apps/Settings" "$SCRIPT_DIR/android_packages_apps_Settings--cutout-force-full-screen.patch"
