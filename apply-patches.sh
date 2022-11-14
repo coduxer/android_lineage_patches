41b14265b7d9516502d81f5e60aaea6176785fa0
coduxer <coduxer@gmail.com>
2022-11-14 20:53:30 +0200

    lineage-patches: add missing patches to apply and clean
    Change-Id: I368cfa0561822d49abb0a306b2187dcd10d73215

--- apply-patches.sh
+++ apply-patches.sh
@@ -17,3 +17,12 @@ clean-repo "packages/modules/Permission"
 apply-patch "packages/modules/Permission" "$SCRIPT_DIR/packages_modules_Permission-S.patch"
 clean-repo "system/core"
 apply-patch "system/core" "$SCRIPT_DIR/android_system_core-safetynetfix.patch"
+
+clean-repo "vendor/lineage"
+# apply-patch "vendor/lineage" "$SCRIPT_DIR/android_vendor_lineage-custom-display-interface.patch"
+apply-patch "vendor/lineage" "$SCRIPT_DIR/android_vendor_lineage--bromite.patch"
+
+clean-repo "lineage-sdk"
+clean-repo "packages/apps/Settings"
+"$SCRIPT_DIR/features/3_fingers_swipe_screenshot/apply-patch.sh"
+"$SCRIPT_DIR/features/cutout_force_full_screen/apply-patch.sh"
